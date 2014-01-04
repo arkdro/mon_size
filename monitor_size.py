4abe1b6eae5c6e42b8043c3e1e264a5d78bbd026
Arkadiy Drovosekov <arkdro@gmail.com>
2014-01-04 11:46:30 +0400

    calc points per inch

--- monitor_size.py
+++ monitor_size.py
@@ -1,5 +1,6 @@
 # given a diagonal and a sides ratio
 # calculate sides of a monitor
+# and points per inch
 
 import argparse
 import numpy as np
@@ -24,17 +25,32 @@ def conv_ratio(r0):
         ratio = float(r0)
     return ratio
 
+def calc_ppi(w, h, points_w, points_h):
+    if points_w and points_h:
+        ppi_w = int(points_w / w)
+        ppi_h = int(points_h / h)
+        return ppi_w, ppi_h
+
 def main():
     parser = argparse.ArgumentParser(description='sizes of a monitor')
     parser.add_argument('-d', '--diag', dest='diag', type=float,
                        required=True, help='diagonal')
     parser.add_argument('-r', '--ratio', dest='ratio',
                        required=True, help='ratio')
+    parser.add_argument('-pw', '--points-width', dest='width', type=int,
+                       help='width in points')
+    parser.add_argument('-ph', '--points-height', dest='height', type=int,
+                       help='height in points')
     args = parser.parse_args()
     # print(args)
     ratio = conv_ratio(args.ratio)
     (w, h) = sides(args.diag, ratio)
-    print w, h
+    ppi = calc_ppi(w, h, args.width, args.height)
+    if ppi:
+        (ppi_w, ppi_h) = ppi
+        print w, h, ppi_w, ppi_h
+    else:
+        print w, h
 
 if __name__ == '__main__':
     main()
